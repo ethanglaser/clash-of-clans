4ccedc7a15cd3405a43fd6f9257f007b5a769d95
ethanglaser <glasere@purdue.edu>
2020-04-29 00:17:12 -0500

    organizing, command line args, documentation

--- README.md
+++ README.md
@@ -9,10 +9,26 @@ These JavaScript files allow for organization of clan and account achievements, 
 
 The first step is to create an account for the API. In order to use the API, authorization is needed using a JSON Web Token, which can be generated [here](https://developer.clashofclans.com/#/account). In order to generate the token, the user must be logged into their developer account and include the IP address of the device that will be used to access the API.
 
+The node *requests* library is required for this repository. Running *npm install requests* in the terminal will install this.
+
 # API features
 
-The base API URL is *https://api.clashofclans.com/v1/*. All API calls will be based off of this base.
+The base API URL is *https://api.clashofclans.com/v1/*. All API calls will be based off of this base. Substitute '#' with '%23' when making an API call. Information on what sorts of data are accessible [here](https://developer.clashofclans.com/#/documentation). Essentially, anything visible within a player or clan's public profile are accessible by the API.
 
-Substitute '#' with '%23'
+# Running the code
 
-npm install requests
+All of the code for this project is currently in *clanleaderboards.js*. This code takes a list of different achievements and profile features and creates leaderboards for each, accounting for each member of a specified clan. Command line arguments are needed for the API token and clan tag, as well as an optional file name.
+
+The format looks like *node clanleaderboards.js [TOKEN HERE] [CLAN TAG HERE] [FILE NAME HERE (optional)].
+
+example: *node clanleaderboards.js abcdefghijklmnopqrstuvwxyz #ABCD1234 ClanEthan*
+
+If no argument is provided for file name, it will default to *leaderboards*.
+
+The code itself involves two levels of API calls, sorting/analysis of data, and the creation of the output file. The command line arguments are processed and then an API call is made to get the members of the specified clan. Then, additional API calls are made for each player to get their achievement data, current season data, etc. Once the data is compiled for each player, the information for each category is sorted, organized, and printed to the output file. Comments are provided throughout the file for further clarification.
+
+Additional leaderboard categories can be added simply by appending the keys at the top of the file, depending on the type of category. Note that the value in either the achievementCategory or baseCategory must match exactly as it is in the API, but the text displayed for the category can be changed by adding a key-value pair to the categoryKey.
+
+The current format of the output is suited best for text file generation, but can be modified for CSV/Excel use by implementing the comments towards the end of the *analysis* function.
+
+Assuming all goes correctly, a text file will be created with leaderboards for every category specified, for the clan provided with the tag in the command line arguments.
--- clanleaderboards.js
+++ clanleaderboards.js
@@ -1,124 +1,157 @@
-var key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU2YWUyMDMyLTcwNjEtNDQ1MS04NTgyLWRhYzlkNTQ2OWYyZCIsImlhdCI6MTU4NzcwODkwNiwic3ViIjoiZGV2ZWxvcGVyLzJmMmZhYTk2LTc2NTItNmZjYi0zMTJhLTk5MzM0OGY3ZGU0MCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjczLjE2NC40Mi42MCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.9zb2g8-7742PUy6EPjfn8TgteJuvjMfEIoKGvGQMtbRda6iasUelNKttR7NcdttzF-2txpvBskrUFtMW1KG4Dw'
 var base = 'https://api.clashofclans.com/v1/'
-var chroma = '%23PURPCR90'
 
 const request = require('request');
 const request2 = require('request');
 const fs = require('fs');
 
+//additional leaderboards can be added simply by adding the category to the appropriate list - anything from the base profile should be added to baseCategories, achievements added to achievementCategories, and optionally add a description that will be displayed in the text file, or modify the existing keys
 var achievementCategories = ["Friend in Need", "Gold Grab", "Elixir Escapade", "Conqueror", "Unbreakable", "Heroic Heist"];
 var baseCategories = ["expLevel", "warStars", "bestTrophies", "bestVersusTrophies"];
 var categoryKey = {"expLevel": "Experience Level", "warStars": "War Stars", "bestTrophies": "Highest Trophy Count", "bestVersusTrophies": "Highest Versus Trophy Count", "Friend in Need": "Total Donations", "Gold Grab": "Gold Stolen (Capped at 2 billion)", "Elixir Escapade": "Elixir Stolen (Capped at 2 billion)", "Conqueror": "Multiplayer Attacks Won", "Unbreakable": "Multiplayer Defenses Won", "Heroic Heist": "Dark Elixir Stolen"};
 var playersInClan = {};
 var clanName = '';
 
+//implement command line arguments
+if (process.argv.length < 4) {
+  throw new Error('Please include token and clan tag in command line arguments');
+}
+var key = process.argv[2];
+var clanTag = process.argv[3].replace('#', '%23');
+if (process.argv.length > 4) {
+  fileName = process.argv[4] + '.txt';
+}
+else {
+  fileName = 'leaderboards.txt';
+}
+fs.unlink(fileName, function(err){});  //Delete file if it already exists
 
-  function formatter(rank, name, value) {
-    line = '';
-    line =+ String(rank) + '.  '
-    if (rank < 10) {
-      line += ' ';
+requesting();
+
+//format the lines of the text file to line up the rank, name and value properly
+//Note that names with emojis or non-ASCII characters will likely disrupt the organization
+function formatter(rank, name, value) {
+  line = String(rank) + '.  '
+  if (rank < 10) {
+    line += ' ';
+  }
+  line += name;
+  line += String(value).padStart(35 - line.length, ' ');
+
+  fs.appendFileSync(fileName, line + '\n', function (err) {
+    if (err) throw err;
+  });
+}
+
+//takes the data for each achievement and sorts it in order
+function analysis() {
+  categories = baseCategories.concat(achievementCategories);
+
+  //print text file header
+  fs.appendFileSync(fileName, 'Leaderboards for ' + clanName + '\n', function (err) {
+    if (err) throw err;
+  });
+
+  for (index in categories) {
+    //create dictionary of name:value for each achievement (categoryDict) and sorted list of all values (rankedValues)
+    categoryDict = {};
+    category = categories[index];
+    for (player in playersInClan){
+      categoryDict[playersInClan[player].Name] = Number(playersInClan[player][category]);
     }
-    line += name;
-    if (name == 'bball ( ͡° ͜ʖ ͡') {
-      line += String(value).padStart(38 - line.length, ' ');
+    rankedValues = new Set(Object.values(categoryDict).sort((a, b) => b - a));
+    
+    //print header for each achievement based on categoryKey
+    if (categories[index] in categoryKey) {
+      fs.appendFileSync(fileName, '\n' + categoryKey[categories[index]] + ':\n', function (err) {
+        if (err) throw err;
+      });   
     }
     else {
-      line += String(value).padStart(35 - line.length, ' ');
+      fs.appendFileSync(fileName, '\n' + categories[index] + ':\n', function (err) {
+        if (err) throw err;
+      });        
     }
-    fs.appendFileSync('leaderboards.txt', line + '\n', function (err) {
-      if (err) throw err;
-    });
-  }
 
-  function analysis() {
-    categories = baseCategories.concat(achievementCategories);
-    fs.appendFileSync('leaderboards.txt', 'Leaderboards for ' + clanName + '\n', function (err) {
-      if (err) throw err;
-    });
-    for (index in categories) {
-      categoryDict = {};
-      category = categories[index];
-      for (player in playersInClan){
-        categoryDict[playersInClan[player].Name] = Number(playersInClan[player][category]);
-      }
-      rankedValues = new Set(Object.values(categoryDict).sort((a, b) => b - a));
-      clanRank = 1;
-      if (categories[index] in categoryKey) {
-        fs.appendFileSync('leaderboards.txt', '\n' + categoryKey[categories[index]] + ':\n', function (err) {
-          if (err) throw err;
-        });   
-      }
-      else {
-        fs.appendFileSync('leaderboards.txt', '\n' + categories[index] + ':\n', function (err) {
-          if (err) throw err;
-        });        
-      }
-
-      rankedValues.forEach(val => {
-        numberAtValue = 0;
-        for (player in categoryDict) {
-          if (categoryDict[player] == val) {
-            formatter(clanRank, player, val)
-            numberAtValue += 1;
-          }
+    //send the data to be formatted and printed to the text file
+    clanRank = 1;
+    rankedValues.forEach(val => {
+      numberAtValue = 0;
+      for (player in categoryDict) {
+        if (categoryDict[player] == val) {
+          formatter(clanRank, player, val)
+          //Can make it more conducive to creating CSV files if desired - comment out the formatter call and uncomment these 3 lines
+          /*fs.appendFileSync(fileName', clanRank + ',' + player + ',' + val + '\n', function (err) {
+            if (err) throw err;
+          });*/  
+          numberAtValue += 1;
         }
-        clanRank += numberAtValue;
-      });
+      }
+      clanRank += numberAtValue;
+    });
   }
 }
 
-  function requesting2(options, status) {
-    request2(options, (error, response, body) => {
-          const info = JSON.parse(body);
-          clanName = info["clan"]["name"];
-          for (const base in info) {
-            if (baseCategories.includes(base)) {
-              playersInClan[info.tag][base] = info[base]
-            }
-          }
-          for (const achievement in info.achievements) {
-            if (achievementCategories.includes(info.achievements[achievement].name)) {
-              playersInClan[info.tag][info.achievements[achievement].name] = info.achievements[achievement].value;
-            }
-          }
-          if (status) {
-            setTimeout(analysis, 2000);
-          }
-      });
-  };
-  function requesting() {
-      const options = {
-          url: base.concat('clans/', chroma, '/members'),
-          headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'Authorization': 'Bearer '.concat(key)
-          }
-        };
-    request(options, (error, response, body) => {
-        const info = JSON.parse(body);
-        counter1 = 0;
-        for (const clanPlayer in info.items) {
-          counter1 += 1;
-          playersInClan[info.items[clanPlayer]["tag"]] = {};
-          playersInClan[info.items[clanPlayer]["tag"]]['Name'] = info.items[clanPlayer]["name"];
-        }
-        counter = 0;
-        for (const tag in playersInClan) {
-          counter += 1;
-          status = (counter == counter1);
-          const options2 = {
-            url: base.concat('players/', tag.replace('#', '%23')),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer '.concat(key)
-            }
-          };
-          requesting2(options2, status);
-        }
-    });
+function requesting2(options, status) {
+  request2(options, (error, response, body) => {
+    const info = JSON.parse(body);
+    clanName = info["clan"]["name"];
+
+    //add data from main profile
+    for (const base in info) {
+      if (baseCategories.includes(base)) {
+        playersInClan[info.tag][base] = info[base]
+      }
+    }
+
+    //add data from achievements list
+    for (const achievement in info.achievements) {
+      if (achievementCategories.includes(info.achievements[achievement].name)) {
+        playersInClan[info.tag][info.achievements[achievement].name] = info.achievements[achievement].value;
+      }
+    }
+
+    //times out to account for API data delay - this is scrappy and can definitely be improved
+    if (status) {
+      setTimeout(analysis, 2000);
+    }
+  });
 };
 
-requesting();
+function requesting() {
+  //initialize API call
+  const options = {
+    url: base.concat('clans/', clanTag, '/members'),
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '.concat(key)
+    }
+  };
+
+  request(options, (error, response, body) => {
+    const info = JSON.parse(body);
+    counter, counter1 = 0, 0;
+
+    //initialize dict to tag:name
+    for (const clanPlayer in info.items) {
+      counter1 += 1;
+      playersInClan[info.items[clanPlayer]["tag"]] = {};
+      playersInClan[info.items[clanPlayer]["tag"]]['Name'] = info.items[clanPlayer]["name"];
+    }
+
+    //player API call to get more data on each player in the clan
+    for (const tag in playersInClan) {
+      counter += 1;
+      status = (counter == counter1);
+      const options2 = {
+        url: base.concat('players/', tag.replace('#', '%23')),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '.concat(key)
+        }
+      };
+      requesting2(options2, status);
+    }
+  });
+};
